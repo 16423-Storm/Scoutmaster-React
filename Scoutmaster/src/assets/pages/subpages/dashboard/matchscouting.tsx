5ba2cc83ac937f0459780174ba1806109b33736f
abcde456 <yellieth2575@hwdsb.on.ca>
2026-08-06 09:23:18 -0400

    Added match selection page for match scouting

--- Scoutmaster/src/assets/pages/subpages/dashboard/matchscouting.tsx
+++ Scoutmaster/src/assets/pages/subpages/dashboard/matchscouting.tsx
@@ -1,5 +1,121 @@
+import { useScreenType } from "../../../scripts/multipageutils";
+import { useTranslation } from "react-i18next";
+
+import { useMatches } from "../../../scripts/localstorage";
+
 function DashboardMatchScouting() {
-    return <>match scouting</>;
+    const { t } = useTranslation();
+    const matches = useMatches((state) => state.matches);
+
+    if (useScreenType() == "desktop") {
+        return (
+            <div
+                className="desktop-dash-comp-infodisplay"
+                style={{ width: "85%" }}
+            >
+                <p className="desktop-dash-comp-infodisplay-title">
+                    {t("totalmatches", {
+                        num: Object.keys(matches ?? {}).length,
+                    })}
+                </p>
+                <div className="desktop-dash-comp-infodisplay-matchtable">
+                    <div className="desktop-dash-comp-infodisplay-matchtable-row">
+                        <div className="desktop-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("match")}
+                        </div>
+                        <div className="desktop-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("red1")}
+                        </div>
+                        <div className="desktop-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("red2")}
+                        </div>
+                        <div className="desktop-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("blue1")}
+                        </div>
+                        <div className="desktop-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("blue2")}
+                        </div>
+                    </div>
+                    {Object.entries(matches).map(([matchNum, match]) => (
+                        <div
+                            className="desktop-dash-comp-infodisplay-matchtable-row"
+                            key={matchNum}
+                        >
+                            <div className="desktop-dash-comp-infodisplay-matchtable-column1">
+                                Q{matchNum}
+                            </div>
+                            <div className="desktop-dash-comp-infodisplay-matchtable-column23">
+                                {match.red1}
+                            </div>
+                            <div className="desktop-dash-comp-infodisplay-matchtable-column23">
+                                {match.red2}
+                            </div>
+                            <div className="desktop-dash-comp-infodisplay-matchtable-column45">
+                                {match.blue1}
+                            </div>
+                            <div className="desktop-dash-comp-infodisplay-matchtable-column45">
+                                {match.blue2}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div
+                className="phone-dash-comp-infodisplay"
+                style={{ padding: "10px 2px", maxHeight: "95%" }}
+            >
+                <p className="phone-dash-comp-infodisplay-title">
+                    {t("totalmatches", {
+                        num: Object.keys(matches ?? {}).length,
+                    })}
+                </p>
+                <div className="phone-dash-comp-infodisplay-matchtable">
+                    <div className="phone-dash-comp-infodisplay-matchtable-row">
+                        <div className="phone-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("match")}
+                        </div>
+                        <div className="phone-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("red1")}
+                        </div>
+                        <div className="phone-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("red2")}
+                        </div>
+                        <div className="phone-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("blue1")}
+                        </div>
+                        <div className="phone-dash-comp-infodisplay-matchtable-column2345header">
+                            {t("blue2")}
+                        </div>
+                    </div>
+                    {Object.entries(matches).map(([matchNum, match]) => (
+                        <div
+                            className="phone-dash-comp-infodisplay-matchtable-row"
+                            key={matchNum}
+                        >
+                            <div className="phone-dash-comp-infodisplay-matchtable-column1">
+                                Q{matchNum}
+                            </div>
+                            <div className="phone-dash-comp-infodisplay-matchtable-column23">
+                                {match.red1}
+                            </div>
+                            <div className="phone-dash-comp-infodisplay-matchtable-column23">
+                                {match.red2}
+                            </div>
+                            <div className="phone-dash-comp-infodisplay-matchtable-column45">
+                                {match.blue1}
+                            </div>
+                            <div className="phone-dash-comp-infodisplay-matchtable-column45">
+                                {match.blue2}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default DashboardMatchScouting;
